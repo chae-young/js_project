6c4670a6434f7d4774e5043f10139fb3c360f5f3
chae-young <cycy8527@gmail.com>
2020-09-07 16:27:00 +0900

    ing

--- algorithm/lv1/모의고사.js
+++ algorithm/lv1/모의고사.js
@@ -27,10 +27,32 @@ function solution(answers) {
     var person2 = [2,1,2,3,2,4,2,5];
     var person3 = [3,3,1,1,2,2,4,4,5,5];
 
-    //답의 길이만큼 답 제출
+	var person1Arr = [];	
+	var person2Arr = [];
+	var person3Arr = [];
+
+    //답의 길이만큼 답 담기
     for(var i = 0;i < answers.length;i++){
-        console.log(person1[i%5],person2[i%8],person3[i%9])
+		person1Arr.push(person1[i%5])
+		person2Arr.push(person2[i%8])
+		person3Arr.push(person3[i%9])
     }
+     //console.log(person1Arr,person2Arr,person3Arr)
+	var ans1 = [];
+	var ans2 = [];
+	var ans3 = [];
+
+	//답 서로 맞는지 확인. 맞으면 1 틀리면 0
+	for(var j = 0;j < answers.length;j++){
+		//if(answers[j] == person1Arr[j]){
+			ans1.push( Number(answers[j] == person1Arr[j]) || Number(answers[j] == person1Arr[j]) )
+			ans2.push( Number(answers[j] == person2Arr[j]) || Number(answers[j] == person2Arr[j]) )
+			ans3.push( Number(answers[j] == person3Arr[j]) || Number(answers[j] == person3Arr[j]) )
+		//}else{
+			//ans.push(0)			
+		//}
+	}
+	console.log(ans1,ans2,ans3)
 
 
     //return person1
