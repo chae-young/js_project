f05f7cb9fdc0ac926bab77ec59bbd0048797d302
chae-young <cycy8527@gmail.com>
2021-01-14 17:46:42 +0900

    기초

---
+++ algorithm/goorm/lv1/hello_goorm.js
@@ -0,0 +1,41 @@
+/*
+
+// Run by Node.js
+
+const readline = require("readline");
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+
+
+rl.on("line", function(n) {
+
+	const txt = 'Hello Goorm !'
+	for(let i = 0;i < n; i++){
+		console.log(txt)
+	}
+
+	rl.close();
+}).on("close", function() {
+	process.exit();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
