b3abde3b94245311809fdcc190c25159925f963f
이채영 <cycy8527@gmail.com>
2021-04-06 22:56:09 +0900

    두번째시도

--- algorithm/programers/lv1/secret_map.js
+++ algorithm/programers/lv1/secret_map.js
@@ -9,16 +9,16 @@ function solution(n, arr1, arr2) {
     arr2.forEach(element => {
         arr2Binary.push(parseInt(element.toString(2).padStart(5,0)))
     });   
-    
-    for(let i = 0; i<=n; i++){
+
+    for(let i = 0; i<n; i++){
         let sumVal = String(arr1Binary[i] + arr2Binary[i]);
-        if(sumVal.length < 6) sumVal = 0+sumVal 
+        if(sumVal.length < n) sumVal = 0+sumVal 
         sumArr.push(sumVal);
     }
 
     const answer = sumArr.map((element)=>{
         let tag = '';
-        for(let i = 0;i<=element.length;i++){
+        for(let i = 0;i<element.length;i++){
             console.log(element)
             if(element[i]>0){
                 tag = tag.concat('#')        
@@ -35,4 +35,4 @@ function solution(n, arr1, arr2) {
 }
 
 console.log( solution(5,[9, 20, 28, 18, 11],[30, 1, 21, 17, 28]) )
-console.log( solution(5,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]) )
+console.log( solution(6,[46, 33, 33 ,22, 31, 50],[27 ,56, 19, 14, 14, 10]) )
