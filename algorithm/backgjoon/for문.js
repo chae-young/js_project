b0b8e77720f7824bb9427041e71f08f3dd5d6dc3
이채영 <cycy8527@gmail.com>
2021-11-06 16:54:45 +0900

    Add for문

--- algorithm/backgjoon/for문.js
+++ algorithm/backgjoon/for문.js
@@ -52,3 +52,50 @@ for(let i = 5; i >= 1; i--){
     sum += i + '\n'
 }
 console.log(sum)
+
+// a+B-7
+const [n3,...arr3] = ['5',
+'1 1',
+'2 3',
+'3 4',
+'9 8',
+'5 2']
+//Case #1: 1 + 1 = 2
+for(let i = 0; i < n3; i++){
+    const [a2,b2] = arr3[i].split(' ')
+    console.log(`Case #${i+1}: ${a2} + ${b2} = ${a2 + b2}`)
+}
+
+//별
+const a3 = 5;
+for(let i = 1;i <= +a3; i++){
+    console.log('*'.repeat(i))
+}
+//별 오른쪽
+const a4 = 5;
+
+for(let i = a4; i >= 1; i--){
+    let str = ' '.repeat(i-1) //4 3
+    let star = '*'.repeat(a4 - str.length) //1 1
+    console.log(str+star)
+}
+// 다른풀이
+// for(i=1;i<=c;i++)console.log(' '.repeat(c-i)+'*'.repeat(i))
+
+
+
+//x보자 작은수
+const [num,arr4] = ['10 5',
+'1 10 4 9 2 3 8 5 7 6']
+
+const [t,x] = num.split(' ')
+const arrA = arr4.split(' ')
+let result = ''
+for(let i = 0; i < +t; i++){
+    if(arrA[+i] < +x){
+        result += arrA[+i] + ' '
+    }
+}
+console.log(result)
+
+// 필터함수를 쓸생각을 왜 안했지 .. ㅂㅂ
