0d6b5159585cd12f643680d9e6f59839c7762b3f
이채영 <cycy8527@gmail.com>
2021-11-13 00:08:03 +0900

    test 수정

--- algorithm/backgjoon/while.js
+++ algorithm/backgjoon/while.js
@@ -11,6 +11,15 @@ let answer = ""
 while(v>0){ // 1>0
     const [n1,n2] = input[i].split(' ') // '1 1'
     i++
-    answer += +n1 + +n2
-    if(v===0) console.log(answer)
+    v = +n1
+    if(v===0) {
+        console.log(answer) 
+        break
+    };
+    answer += +n1 + +n2 + '\n'
 }
+
+
+// 다른방법
+input.map(v=>v.split(' ')).forEach(v=>
+    +v[0] ? console.log(+v[0]+ +v[1]) : 0)
