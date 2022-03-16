4075c4ff06ecac55a47846ee9cfe45d5dc7515f1
chae-young <cycy8527@gmail.com>
2022-03-16 23:55:47 +0900

    고차함수

--- 2021basic/four/heightOrderFunction.js
+++ 2021basic/four/heightOrderFunction.js
@@ -23,3 +23,32 @@ const innerFunc2 = func2(); //함수를 호출한 상태 return 값이 들어감
 
 innerFunc1();
 innerFunc2();
+
+//커링함수
+var curry = function (func) {
+    return function (a) {
+        return function (b) {
+            return func(a, b);
+        };
+    };
+};
+
+var getMaxWith10 = curry(Math.max)(10);
+console.log(getMaxWith10(8)); // 10
+console.log(getMaxWith10(25)); // 25
+
+var getMinWith10 = curry(Math.min)(10);
+console.log(getMinWith10(8)); // 8
+console.log(getMinWith10(25)); // 10
+
+const sum = (x) => {
+    return (y) => {
+        return (c) => {
+            return x + y + c;
+        };
+    };
+};
+
+const a = sum(10);
+const b = a(5);
+b(5);
